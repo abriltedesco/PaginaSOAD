08b4ff447e5abe2250b994482631f48e4b98ddc0
Abril Tedesco <167026793+abriltedesco@users.noreply.github.com>
2025-07-15 22:29:55 -0300

    ultimas modif

--- node.js
+++ node.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 5500;
 
 // lee datos del form
 app.use(bodyParser.urlencoded({ extended: true }));
